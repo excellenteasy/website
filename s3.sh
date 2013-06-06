e1a30c6be6a6172c995497d04afa474cfd21ed57
Stephan Bönnemann <stephan@boennemann.me>
2013-06-06 13:07:21 +0200

    fix #8 include disqus comments with intelligent load

--- s3.sh
+++ s3.sh
@@ -51,7 +51,7 @@ s3cmd sync --acl-public --mime-type "text/javascript" --add-header="Content-Enco
   --add-header "Cache-Control: private, max-age=31536000" \
   --add-header "Expires: 31536000" \
   --add-header "Vary: Accept-Encoding" \
-  build/blog/assets/js/c-2.js.gz s3://excellenteasy.com/website/blog/assets/js/c-2.js
+  build/blog/assets/js/c-3.js.gz s3://excellenteasy.com/website/blog/assets/js/c-3.js
 
 s3cmd sync --acl-public --mime-type "text/javascript" --add-header="Content-Encoding: gzip" \
   --add-header "Cache-Control: private, max-age=31536000" \
--- src/blog/assets/_js/custom.js
+++ src/blog/assets/_js/custom.js
@@ -34,3 +34,20 @@ registerMatchForWidth(319, 150);
 enquire.register("screen and (min-width:700px)", function() {
   changeImgSrcPostfixTo(660);
 });
+
+// Disqus
+if (window.disqus_title) {
+  var $window = $(window);
+  var treshold = ($('body').height()*0.7);
+  var loadDisqus = function(e) {
+    if ($window.scrollTop() >= treshold) {
+      $window.off('scroll', loadDisqus);
+      (function() {
+        var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+        dsq.src = 'http://' + disqus_shortname + '.disqus.com/embed.js';
+        (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+      })();
+    }
+  };
+  $window.on('scroll', loadDisqus);
+}
