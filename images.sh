9bd166a86bd89032d30f2a1fea8c233440d70e22
David Pfahler <david@excellenteasy.com>
2013-07-21 17:38:05 +0200

    public cache control headers

--- images.sh
+++ images.sh
@@ -3,7 +3,7 @@ set -x verbose
 
 # blog content images
 s3cmd sync --acl-public  --guess-mime-type \
-  --add-header "Cache-Control: private, max-age=31536000" \
+  --add-header "Cache-Control: public, max-age=31536000" \
   --add-header "Expires: 31536000" \
   -r build/blog/images s3://excellenteasy.com/website/blog/
 
